c4c4bef3463118b17de99f49db865a079c23f562
Lhtwasd22vs33 <1438574779@qq.com>
2022-03-31 16:03:02 +0800

    添加更多参数

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# simple-fireworks 【简单的烟花】
+# <p align="center">simple-fireworks 【简单的烟花】</p>
 
-#### 帮助你简单快捷的生成烟花效果,你只需要给予canvas对象和一些简单的参数
+<br>
 
+#### <p align="center">简单快捷的在js中生成烟花效果 <br> 拥有很强的自定义效果,你可以自定义出属于你的烟花效果 </p>
 
+<br>
 
 ### 使用方法：
 
@@ -16,23 +18,8 @@ import fireworks from 'simple-fireworks'
 ````
 
 ````
-const canvas = document.querySelector('canvas')
 
-//在创建实例时需要传入canvas对象，如果不传，默认会在body内创建canvas
-const new_fireworks = new fireworks(canvas)
-
-// 检测浏览器宽高变化
-window.addEventListener('resize', () => {
-    changeCanvas()
-}, false);
-// 设置画布宽高
-changeCanvas()
-
-//这里的值*4是为了让绘制出的粒子更细腻
-function changeCanvas() {
-    canvas.width = window.innerWidth * 4;
-    canvas.height = window.innerHeight * 4;
-}
+const new_fireworks = new fireworks()
 
 // 获取点击坐标
 document.addEventListener('mousedown', (e) => {
@@ -49,31 +36,48 @@ document.addEventListener('mousedown', (e) => {
 }, false);
 ````
 
+<br>
+
 ### draw参数说明:
-| 参数名 | 必填 | 值 | 默认值 | 描述 |
-| :---: | :---: | :---: | :---: | :---: |
-|x|true|Number||canvas对应的x坐标|
-|y|true|Number||canvas对应的x坐标|
-|count||Number|100|生成粒子的数量|
-|radius||Number|100|半径|
-|drop||Number|0|下坠速度|
-|hue||Number|0-360的随机数|色相|
-|hueVariance||Number|0|色调变化|
-|saturations||String|'100%'|饱和度|
-|brightnessMin||Number|50|最小亮度|
-|brightnessMax||Number|80|最大亮度|
-|alphaMin||Number|40|最小透明度|
-|alphaMax||Number|100|最大透明度|
-|color||Array['String']|['']|颜色数组 使用后上述的颜色属性<kbd>hue</kbd>,<kbd>hueVariance</kbd>,<kbd>saturations</kbd>,<kbd>brightnessMin</kbd>,<kbd>brightnessMax</kbd>,<kbd>alphaMin</kbd>,<kbd>alphaMax</kbd>自动失效|
-|angleStart||Number|0|开始角度|
-|angleEnd||Number|359|结束角度|
-|speedMin||Number|0.5|最小扩散速度|
-|speedMax||Number|14.5|最大扩散速度|
-|acceleration||Number|0|加速度(溅射效果)效果是让开始的速度加快,并且运动距离更远|
-|sizeMin||Number|1|最小粒子|
-|sizeMax||Number|3|最大粒子|
-|dissolve||Number:1~100|5|溶解,消失速度|
+
+> 这里的范围只是我个人推荐使用的值的范围,代码内部并未进行参数值是否在范围内的判断,所以除了x,y以外,请最好不要将参数的值设置为0等判断为false的值,如果设置为0,将使用默认值.
+
+| 参数名 | 必填 | 值 |范围| 默认值 | 描述 |
+| :---: | :---: | :---: | :---: | :---: | :---: |
+|x*|true|Number|||canvas对应的x坐标|
+|y*|true|Number|||canvas对应的x坐标|
+|count||Number||100|生成粒子的数量|
+|radius||Number||100|半径|
+|drop||Number||0|下坠速度|
+|hue||Number||0-360的随机数|色相|
+|hueVariance||Number||0|色调变化|
+|saturations||String||'100%'|饱和度|
+|brightnessMin||Number||50|最小亮度|
+|brightnessMax||Number||80|最大亮度|
+|alphaMin||Number||40|最小透明度|
+|alphaMax||Number||100|最大透明度|
+|angleStart||Number||0|开始角度|
+|angleEnd||Number||359|结束角度|
+|speedMin||Number||0.5|最小扩散速度|
+|speedMax||Number||14.5|最大扩散速度|
+|acceleration||Number||0|加速度(溅射效果) 效果是会让一开始的速度很快,并且运动距离更远|
+|sizeMin||Number||1|最小粒子|
+|sizeMax||Number||3|最大粒子|
+|dissolve||Number:1~100||5|溶解,消失速度|
+|erasingSpeed||Number||10|擦除速度,速度越慢拖尾越长|
+|is_line||Boolean|true or false|false|是否从底部生成线条飞出|
+|line_size||Number||2|线条粗细|
+|ling_speed||Number||4|线条向上的速度|
+|two_explode||Boolean||false|是否二次爆开|
+|two_obj||object||{}|第二次爆开的烟花参数 参数内容同上,这里的x,y,值为第一次爆开的粒子的位置|
+
+<br>
+
+### 示例
+
+> [GitHub链接:https://github.com/Lhtwasd22vs33/simple-fireworks](https://github.com/Lhtwasd22vs33/simple-fireworks)
+
+<br>
 
 
-### GitHub
-
+### [二次爆炸](https://github.com/Lhtwasd22vs33/simple-fireworks)
--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
+"use strict"
 class fireworks {
-    constructor(canvas) {
-        if(canvas){
-            this.canvas = canvas
-            this.context = this.canvas.getContext('2d');
-        }else{
-            
-        }
+    constructor() {
+        // 动态创建canvas对象
+        this.id="fireworks-canvas"
+        let canvasdom = document.createElement("canvas");
+        canvasdom.id = this.id;
+        canvasdom.style.position = "fixed";
+        canvasdom.style.left = "0";
+        canvasdom.style.top = "0";
+        canvasdom.style.zIndex = -1;
+        canvasdom.style.pointerEvents ='none';
+        document.body.appendChild(canvasdom);
+        this.canvas=document.getElementById(this.id);
         
+        // 获取屏幕像素比
+        this.pixel_ratio=window.devicePixelRatio;
+
+        // 设置canvas宽高
+        this.canvas.width=innerWidth*this.pixel_ratio
+        this.canvas.height=innerHeight*this.pixel_ratio
+
+        // 当里浏览器宽高变化时
+        window.addEventListener('resize', () => {
+            this._changeCanvas()
+        }, false);
+
+        // 创建画布对象
+        this.context = this.canvas.getContext('2d');
+        // 创建烟花对象
         this._fireworks = {}
     }
-    clearCanvas() {
+    clearCanvas(erasingSpeed) {
+        // 清除画布内容
         this.context.globalCompositeOperation = 'destination-out';
-        this.context.fillStyle = 'rgba(0,0,0,' + 10 / 100 + ')';
+        if(erasingSpeed){
+            this.context.fillStyle = 'rgba(0,0,0,' + erasingSpeed / 100 + ')';
+        }else{
+            this.context.fillStyle = 'rgba(0,0,0,' + 10 / 100 + ')';
+        }
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.globalCompositeOperation = 'lighter';
         this._clearCanvas = requestAnimationFrame(() => {
-            this.clearCanvas()
+            this.clearCanvas(erasingSpeed)
         })
+    }
+    _changeCanvas(){
+        this.canvas.width=innerWidth*this.pixel_ratio
+        this.canvas.height=innerHeight*this.pixel_ratio
     }
     draw(obj) {
         if (!this._clearCanvas) {
-            this.clearCanvas()
+            this.clearCanvas(obj.erasingSpeed)
         }
 
         if (!this._fireworks) {
@@ -33,18 +63,54 @@ class fireworks {
 
         _obj.key = this.key
 
-        this._init_1(_obj)
+        this._init(_obj)
     }
-    _init_1(obj){
-        this._fireworks[obj.key] = {
-            particles: []
+    _init(obj){
+        if(obj.is_line){
+            this._fireworks[obj.key] = {
+                particles: [],
+                line_x:obj.x,
+                line_y:window.innerHeight,
+                line_top:obj.y,
+                two_explode:obj.two_explode||false
+            }
+        }else{
+            this._fireworks[obj.key] = {
+                particles: [],
+                two_explode:obj.two_explode||false
+            }
         }
+        
         this._createFireworks(obj)
+        if(obj.is_line){
+            this.line_to_Top(obj)
+        }else{
+            this._drawFireworks(obj)
+        }
+        
+    }
+    _init_2(obj){
+        this.key = Date.now()
+        obj.key=this.key
+        this._fireworks[obj.key] = {
+            particles: [],
+            two_explode:obj.two_explode||false
+        }
+        for(let i=0 ;i<obj.two_particles.length;i++){
+            obj.x=obj.two_particles[i].x
+            obj.y=obj.two_particles[i].y
+            this._createFireworks(obj)
+        }
+
         this._drawFireworks(obj)
     }
+
     _createFireworks(obj) {
-        this._fireworks[obj.key].particles = []
+        
         let { x, y } = obj
+        if( !x ){throw new Error(`fireworks x值不能为 ${x}`);}
+        if( !y ){throw new Error(`fireworks y值不能为 ${y}`);}
+
         let hue = obj.hue || Math.floor(Math.random() * 361);
         let hueVariance = obj.hueVariance || 0;
         let count = obj.count || 100;
@@ -101,12 +167,41 @@ class fireworks {
             p.dissolve = dissolve
             this._fireworks[obj.key].particles.push(p);
         }
+
+    }
+    // 烟花从底部飞出的动画
+    line_to_Top(obj) {
+        let key =obj.key
+        let line_size =obj.line_size || 2
+        let ling_speed=obj.ling_speed || 6
+
+        this._fireworks[key].line_y -= ling_speed
+        // 绘制
+        this.context.beginPath();
+        this.context.arc(this._fireworks[key].line_x, this._fireworks[key].line_y, line_size, 0, Math.PI * 2, false);
+        this.context.closePath();
+        // 设置颜色  
+        this.context.fillStyle = '#fff';
+        this.context.fill();
+
+        // 判断动画是否结束
+        if (this._fireworks[key].line_y <= this._fireworks[key].line_top) {
+            // 清除动画
+            cancelAnimationFrame(this._fireworks[key].line_am_id)
+            // 播放烟花炸开的动画 
+            this._drawFireworks(obj);
+        } else {
+            // 动画循环
+            this._fireworks[key].line_am_id = requestAnimationFrame(() => { this.line_to_Top(obj) });
+        }
     }
     _drawFireworks(obj) {
         let radius = obj.radius || 100
         let drop = obj.drop || 0
         let saturations = obj.saturations || '100%'
         let is_alpha_0 = false
+        let is_alpha_5 = false
+
         for (let i = 0; i < this._fireworks[obj.key].particles.length; i++) {
             let p = this._fireworks[obj.key].particles[i];
             let vx = Math.cos(p.radians) * p.radius;
@@ -118,16 +213,24 @@ class fireworks {
             if (p.alpha > 0) {
                 is_alpha_0 = true
             }
+            if (p.alpha > 0.5) {
+                is_alpha_5 = true
+            }
             this.context.beginPath();
             this.context.arc(p.x, p.y, p.size, 0, Math.PI * 2, false);
             this.context.closePath();
-
             if (p.color) {
                 this.context.fillStyle = p.color;
             } else {
                 this.context.fillStyle = 'hsla(' + p.hue + ', ' + saturations + ', ' + p.brightness + '%, ' + p.alpha + ')';
             }
             this.context.fill();
+        }
+        if (!is_alpha_5&&this._fireworks[obj.key].two_explode) {
+            let two_obj=JSON.parse(JSON.stringify(obj.two_obj))
+            two_obj.two_particles=this._fireworks[obj.key].particles
+            this._init_2(two_obj)
+            this._fireworks[obj.key].two_explode=false
         }
         if (is_alpha_0) {
             this._fireworks[obj.key].am_id = requestAnimationFrame(() => { this._drawFireworks(obj) });
@@ -136,6 +239,7 @@ class fireworks {
             delete this._fireworks[obj.key]
         }
     }
+ 
 }
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
     module.exports = fireworks;
